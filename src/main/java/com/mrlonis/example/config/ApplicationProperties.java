4e031b7977d74fe79bef50a457fe669ee6738473
Matthew Lonis <mattlonis@gmail.com>
2024-06-29 00:00:50 -0400

    Adding more new fields

--- src/main/java/com/mrlonis/example/config/ApplicationProperties.java
+++ src/main/java/com/mrlonis/example/config/ApplicationProperties.java
@@ -10,4 +10,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class ApplicationProperties {
     private String name;
     private String description;
+    private String version;
 }
