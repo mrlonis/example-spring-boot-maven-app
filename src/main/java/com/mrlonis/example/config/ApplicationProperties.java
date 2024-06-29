d3752bfd40b08c4153b3b0e5122c0355bc770617
Matthew Lonis <mattlonis@gmail.com>
2024-06-28 23:54:56 -0400

    Adding a new field to test linux spotless formatting pre-commit

--- src/main/java/com/mrlonis/example/config/ApplicationProperties.java
+++ src/main/java/com/mrlonis/example/config/ApplicationProperties.java
@@ -9,4 +9,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Setter
 public class ApplicationProperties {
     private String name;
+    private String description;
 }
